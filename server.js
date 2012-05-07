a1cb21fb0cef6426e3e91d00792c49676f73e84f
Marty McAllister <marty@that-london.com>
2012-05-07 20:26:41 +0100

    Lots of refactoring
    Pulled lots of bits into methods, should be easier to add some tests
    now.

--- server.js
+++ server.js
@@ -13,32 +13,49 @@ var client = new ircLib.Client(config.ircServer, config.botName, {
         channels: [config.ircChannel],
 });
 
-
-
 client.addListener('message', function (from, to, message) {
 	console.log("from: " + from);
 	console.log("to: " + to);
 	console.log("message: " + message);
 	
+	if (addressedToBot(message))
+	{
+		var apiLink = getTwitterLink(message);
+		processMessage(message, apiLink);
+	}
+	
+});
+
+function addressedToBot(message)
+{
 	if (message.substring(0, config.botName.length) == config.botName)
 	{
-		//assuming we're addressed "parrot: <link>
-		//oh dear, I think this needs some regex.
-		var twitterLink = message.substring(config.botName.length + 2, message.length);
+		return true;
+	}
+}
 
-		var twitterURL = new url.parse(twitterLink);
-		
-		var id = twitterURL.path.substring(twitterURL.path.lastIndexOf('/'), twitterURL.path.length);
+function getTwitterLink(message)
+{
+	//assuming we're addressed "parrot: <link>
+	//oh dear, I think this needs some regex.
+	var twitterLink = message.substring(config.botName.length + 2, message.length);
+
+	var twitterURL = new url.parse(twitterLink);
 	
-		//now get from twitter
-		var apiLink = 'http://api.twitter.com/1/statuses/show/' + id + '.json';
+	var id = twitterURL.path.substring(twitterURL.path.lastIndexOf('/'), twitterURL.path.length);
+	
+	//generate link
+	var options = {
+		host:  'api.twitter.com',
+		port: twitterURL.port,
+		path: '/1/statuses/show/' + id + '.json' 
+	};
+	
+	return (options);
+}
 
-		var options = {
-			host:  'api.twitter.com',
-			port: twitterURL.port,
-			path: '/1/statuses/show/' + id + '.json' 
-		};
-
+function processMessage(message, options)
+{
 		var request = http.get(options, function(res) {
 			var data = '';
 			
@@ -54,10 +71,7 @@ client.addListener('message', function (from, to, message) {
 			});
 
 		});
-		
-	}
-
-});
+}
 
 //don't die on exceptions..
 process.on('uncaughtException', function(err) {
